c357222cc3194af29055affaea54e27e0586fec8
desarrollo219 <desarrollo2@picolab.com.co>
2024-02-16 05:19:57 -0500

    CargaDatos
    Adición de manejo de datos offline

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
+    public DataJSON misDatos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        string filePat = Application.streamingAssetsPath + "/" + "data1.json";
+
+        if (File.Exists(filePat))
+        {
+            string s = File.ReadAllText(filePat);
+            misDatos = JsonUtility.FromJson<DataJSON>(s);
+            Debug.Log(misDatos.nombre_juego);
+            s = JsonUtility.ToJson(misDatos, true);
+            Debug.Log(s);
+            File.WriteAllText(filePat, s);
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/datos.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Serialization;
+
+[System.Serializable]
+public class DataJSON : System.Object
+{
+
+    [SerializeField]
+    public string nombre_juego;
+    [SerializeField]
+    public string nombre_jugador;
+    [SerializeField]
+    public int nivel;
+    [SerializeField]
+    public int salud;
+    [SerializeField]
+    public string correo;
+
+    public DataJSON()
+    {
+    }
+
+}
