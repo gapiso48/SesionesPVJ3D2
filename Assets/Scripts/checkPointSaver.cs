ac28c6c762f9aa5c5cf9e7265b0dafa13a098c9c
gapiso48 <gapiso48@gmail.com>
2024-03-12 09:00:53 -0500

    Checkpoints

---
+++ Assets/Scripts/checkPointSaver.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkPointSaver : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector3 lastCheckpoint;
+
+    void Start()
+    {
+        lastCheckpoint = this.transform.position;
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        
+    }
+        void OnTriggerEnter (Collider c){
+        if (c.gameObject.tag == "checkPoint"){
+            Debug.Log("CheckPoint!!");
+            lastCheckpoint = this.transform.position;
+        }
+        if (c.gameObject.tag == "trampa"){
+            Debug.Log("Desaparece!!");
+            this.GetComponent<CharacterController>().enabled=false;
+
+            this.gameObject.transform.position = lastCheckpoint;
+            this.GetComponent<CharacterController>().enabled=true;
+        }
+    }
+
+}
